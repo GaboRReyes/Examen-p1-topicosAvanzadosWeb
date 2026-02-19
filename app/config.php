a0a2337b4d77f8bdd17013f7797d48ed5e9d0992
GaboRReyes <160171479+GaboRReyes@users.noreply.github.com>
2026-02-18 19:51:30 -0600

    Conexion a la base de datos y configuracion de variables de entorno

---
+++ app/config.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'db_host' => 'localhost',
+    'db_name' => 'url_shortener',
+    'db_user' => 'root',
+    'db_pass' => 'gajos2004',
+    'base_url' => 'http://localhost/url-shortener/public',
+    'code_length' => 6
+];
---
+++ app/database.php
@@ -0,0 +1,21 @@
+<?php
+
+    class database {
+    private static $instance = null;
+
+    public static function getConnection() {
+        if (self::$instance === null) {
+            $config = require __DIR__ . '/config.php';
+
+            self::$instance = new PDO(
+                "mysql:host={$config['db_host']};dbname={$config['db_name']};charset=utf8",
+                $config['db_user'],
+                $config['db_pass']
+            );
+
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+
+        return self::$instance;
+    }
+}
