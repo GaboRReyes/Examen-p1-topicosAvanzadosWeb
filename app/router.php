41e341f495d81e07da08c80d6e1204d5f4207583
GaboRReyes <160171479+GaboRReyes@users.noreply.github.com>
2026-02-18 21:30:42 -0600

    Correccion de errores, renombramiento de requerimentos

--- app/router.php
+++ app/router.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/controllers/UrlController.php';
+require_once __DIR__ . '/controllers/urlController.php';
 
 class Router {
 
--- public/index.php
+++ public/index.php
@@ -5,12 +5,12 @@ error_reporting(E_ALL);
 
 header("Content-Type: application/json");
 
-require_once __DIR__ . '/../app/Router.php';
+require_once __DIR__ . '/../app/router.php';
 
 Router::handle();
 
 header("Content-Type: application/json");
 
-require_once __DIR__ . '/../app/Router.php';
+require_once __DIR__ . '/../app/router.php';
 
 Router::handle();
