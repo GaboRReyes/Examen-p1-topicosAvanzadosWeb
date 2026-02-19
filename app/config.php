0d669a051fa27588801ce7b34c68c3df56e51355
GaboRReyes <160171479+GaboRReyes@users.noreply.github.com>
2026-02-18 22:17:14 -0600

    Otra vez

--- app/config.php
+++ app/config.php
@@ -5,6 +5,6 @@ return [
     'db_name' => 'url_shortener',
     'db_user' => 'gabriel',
     'db_pass' => '123456',
-    'base_url' => 'http://localhost/api_p1/Examen-p1-topicosAvanzadosWeb',
+    'base_url' => 'http://localhost/api_p1/Examen-p1-topicosAvanzadosWeb/public',
     'code_length' => 6
 ];
