8407f92ac3e56cd7bb87b635bf74dd9dde12cb11
GaboRReyes <160171479+GaboRReyes@users.noreply.github.com>
2026-02-18 21:38:43 -0600

    optimizacion del router

--- app/router.php
+++ app/router.php
@@ -1,29 +1,19 @@
 <?php
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$method = $_SERVER['REQUEST_METHOD'];
 
-require_once __DIR__ . '/controllers/urlController.php';
+$base = '/api_p1/Examen-p1-topicosAvanzadosWeb/public';
+$uri = str_replace($base, '', $uri);
 
-class Router {
+if ($method === 'POST' && $uri === '/api/v1/urls') {
+    UrlController::create();
+}
 
-    public static function handle() {
+elseif ($method === 'GET' && preg_match('#^/api/v1/urls/([a-zA-Z0-9]+)/stats$#', $uri, $matches)) {
+    UrlController::stats($matches[1]);
+}
 
-        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-        $method = $_SERVER['REQUEST_METHOD'];
-
-        if ($method === 'POST' && $uri === '/url-shortener/public/api/v1/urls') {
-            UrlController::create();
-        }
-
-        elseif ($method === 'GET' && preg_match('#/api/v1/urls/([a-zA-Z0-9]+)/stats#', $uri, $matches)) {
-            UrlController::stats($matches[1]);
-        }
-
-        elseif ($method === 'GET' && preg_match('#/url-shortener/public/([a-zA-Z0-9]+)$#', $uri, $matches)) {
-            UrlController::redirect($matches[1]);
-        }
-
-        else {
-            http_response_code(404);
-            echo json_encode(["error" => "Route not found"]);
-        }
-    }
+else {
+    http_response_code(404);
+    echo json_encode(["error" => "Route not found"]);
 }
