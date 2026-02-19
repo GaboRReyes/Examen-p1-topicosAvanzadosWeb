3c818900cab334c76b828945b4e05e513d3f1420
GaboRReyes <160171479+GaboRReyes@users.noreply.github.com>
2026-02-18 20:49:44 -0600

    index y router

---
+++ app/router.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once __DIR__ . '/controllers/UrlController.php';
+
+class Router {
+
+    public static function handle() {
+
+        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        if ($method === 'POST' && $uri === '/url-shortener/public/api/v1/urls') {
+            UrlController::create();
+        }
+
+        elseif ($method === 'GET' && preg_match('#/api/v1/urls/([a-zA-Z0-9]+)/stats#', $uri, $matches)) {
+            UrlController::stats($matches[1]);
+        }
+
+        elseif ($method === 'GET' && preg_match('#/url-shortener/public/([a-zA-Z0-9]+)$#', $uri, $matches)) {
+            UrlController::redirect($matches[1]);
+        }
+
+        else {
+            http_response_code(404);
+            echo json_encode(["error" => "Route not found"]);
+        }
+    }
+}
