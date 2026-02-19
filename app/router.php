0f568e947de44fd06c118b2e394872f14a5fe611
GaboRReyes <160171479+GaboRReyes@users.noreply.github.com>
2026-02-18 22:06:06 -0600

    modificacion del router

--- app/router.php
+++ app/router.php
@@ -2,27 +2,33 @@
 
 require_once __DIR__ . '/controllers/urlController.php';
 
-class Router {
+class Router
+{
+    public static function handle()
+    {
+        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        $method = $_SERVER['REQUEST_METHOD'];
 
-    public static function handle() {
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$method = $_SERVER['REQUEST_METHOD'];
+        $base = '/api_p1/Examen-p1-topicosAvanzadosWeb/public';
+        $uri = str_replace($base, '', $uri);
+        $uri = rtrim($uri, '/');
 
-$base = '/api_p1/Examen-p1-topicosAvanzadosWeb/public';
-$uri = str_replace($base, '', $uri);
+        if ($method === 'POST' && $uri === '/api/v1/urls') {
+            UrlController::create();
+            exit;
 
-if ($method === 'POST' && $uri === '/api/v1/urls') {
-    UrlController::create();
-}
+        } elseif ($method === 'GET' && preg_match('#^/api/v1/urls/([a-zA-Z0-9]+)/stats$#', $uri, $matches)) {
+            UrlController::stats($matches[1]);
+            exit;
 
-elseif ($method === 'GET' && preg_match('#^/api/v1/urls/([a-zA-Z0-9]+)/stats$#', $uri, $matches)) {
-    UrlController::stats($matches[1]);
-}
+        } elseif ($method === 'GET' && preg_match('#^/([a-zA-Z0-9]+)$#', $uri, $matches)) {
+            UrlController::redirect($matches[1]);
+            exit;
 
-else {
-    http_response_code(404);
-    echo json_encode(["error" => "Route not found"]);
-}
-
+        } else {
+            http_response_code(404);
+            echo json_encode(["error" => "Route not found"]);
+            exit;
+        }
     }
 }
