32dc2276bf7b11e975cbe6694b8942d0a7ea03bb
GaboRReyes <160171479+GaboRReyes@users.noreply.github.com>
2026-02-18 21:49:12 -0600

    correccion de datos en las variables de configuracion

--- app/config.php
+++ app/config.php
@@ -3,8 +3,8 @@
 return [
     'db_host' => 'localhost',
     'db_name' => 'url_shortener',
-    'db_user' => 'root',
-    'db_pass' => 'gajos2004',
+    'db_user' => 'gabriel',
+    'db_pass' => '123456',
     'base_url' => 'http://localhost/url-shortener/public',
     'code_length' => 6
 ];
